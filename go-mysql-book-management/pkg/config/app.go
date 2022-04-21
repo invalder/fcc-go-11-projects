41f516e952031dabe2b7e64f5a17f73882f4f13e
nnakarac <nnakarac@student.42bangkok.com>
2022-04-21 13:24:19 +0700

    Done!, Thank you Teacher

--- go-mysql-book-management/pkg/config/app.go
+++ go-mysql-book-management/pkg/config/app.go
@@ -1,22 +1,22 @@
 package config
 
-import(
+import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 var (
-	db * gorm.DB
+	db *gorm.DB
 )
 
-func Connect(){
-	d, err := gorm.Open("mysql", "db_user:p@ssw0rd/simplerest?charset=utf8&parseTime=True&loc=Local")
+func Connect() {
+	d, err := gorm.Open("mysql", "db_user:p@ssw0rd@tcp(192.168.1.100:3306)/simplerest?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
 	db = d
 }
 
-func GetDB() *gorm.DB{
+func GetDB() *gorm.DB {
 	return db
 }
